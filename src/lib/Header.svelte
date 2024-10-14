17d26b4743874e3d898b7eac3756c734696f0528
Hugo Smith (1000125766) <SMITHT8@student.op.ac.nz>
2024-10-15 10:48:30 +1300

    Set up structuire for header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<header>
+<h1>My classwork website</h1>
+<nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Contact</a></li>
+    </ul>
+</nav>
+</header>
+
+<style>
+
+</style>
