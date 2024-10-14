364c2c7fbc533b6b9c1615ddb020a5c1ffc7549a
Hugo <smitht8@student.op.ac.nz>
2024-10-15 11:15:11 +1300

    Pass a prop

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-
+export let headingTitle;
 </script>
 
 <header>
-<h1>My classwork website</h1>
+<h1>{headingTitle}</h1>
 <nav>
     <ul>
         <li><a href="/">Home</a></li>
@@ -14,5 +14,30 @@
 </header>
 
 <style>
-
+    header{
+        font-family: sans-serif;
+        background-color:lightgray;
+        padding: 1em;
+    }
+h1{
+    display: flex;
+    justify-content: center;
+}
+li{
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    border-radius: 1em;
+    border: 2em; border-color:black;
+}
+ul{
+    background-color: darkgray;
+    padding:2em;
+    display: flex;
+    justify-content: space-around;
+}
+a{
+    text-decoration: none;
+    font-size: large;
+}
 </style>
